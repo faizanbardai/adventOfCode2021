d3624e1a6cc1d9d25520d20f47d152ed27be20ff
FayJu <faizan@unetiq.com>
2021-12-11 22:47:24 +0100

    fix day8

--- 2020/day8/puzzle.js
+++ 2020/day8/puzzle.js
@@ -8,36 +8,50 @@ const visited = new Map();
 const puzzleOne = async () => {
     const data = await getData(input).then(transform);
     try {
-        const acc = runInstruction(data[0], 0, data);
+        const response = runInstruction(data[0], 0, data);
+        console.log({ response });
     } catch (error) {
         console.log(error);
     }
 };
-puzzleOne();
+// puzzleOne();
 
-const runInstruction = (line, i, data, acc = 0) => {
-    if (!data[i]) throw Error({ message: `finished at ${acc}`, type: 0 });
-    if (visited.has(i)) throw Error({ message: `acc is ${acc}`, type: 1 });
-    visited.set(i, data);
-    const { ins, dir, num } = line;
-    switch (ins) {
-        case 'nop':
-            acc += runInstruction(data[i + 1], i + 1, data, acc);
-            break;
-        case 'acc':
-            acc = dir === 1 ? acc + num : acc - num;
-            acc += runInstruction(data[i + 1], i + 1, data, acc);
-            break;
-        case 'jmp':
-            const nextInsToRun = dir === 1 ? i + num : i - num;
-            acc += runInstruction(data[nextInsToRun], nextInsToRun, data, acc);
-            break;
-    }
-    return acc;
+const msg = {
+    finished: 'finished',
+    inLoop: 'in loop',
 };
 
-const puzzleTwo = async () => {
+const runInstruction = (line, i, data, res = { acc: 0, msg: null }) => {
+    if (res.msg) {
+        return res;
+    }
+    if (!data[i]) {
+        res.msg = msg.finished;
+        return res;
+    }
+    if (visited.has(i)) {
+        res.msg = msg.inLoop;
+        return res;
+    }
+    visited.set(i, data[i]);
+    const { ins, dir, num } = line;
+    switch (ins) {
+        case 'nop': {
+            return runInstruction(data[i + 1], i + 1, data, res);
+        }
+        case 'acc': {
+            res.acc += dir === 1 ? +num : -num;
+            return runInstruction(data[i + 1], i + 1, data, res);
+        }
+        case 'jmp': {
+            const next = dir === 1 ? i + num : i - num;
+            return runInstruction(data[next], next, data, res);
+        }
+    }
+};
+
+const findFix = async () => {
     const data = await getData(input).then(transform);
     console.log(data);
 };
-// puzzleTwo();
+findFix();
