02625516d1fadaef2bdae772c8464c28e75bb823
FayJu <faizan@unetiq.com>
2021-12-11 23:34:27 +0100

    day 8 part2

--- 2020/day8/puzzle.js
+++ 2020/day8/puzzle.js
@@ -2,26 +2,21 @@ const getData = require('../../helpers/getData');
 const { transform } = require('./transform');
 
 let input = './test.txt';
-// input = './puzzle.txt';
+input = './puzzle.txt';
 
-const visited = new Map();
 const puzzleOne = async () => {
     const data = await getData(input).then(transform);
-    try {
-        const response = runInstruction(data[0], 0, data);
-        console.log({ response });
-    } catch (error) {
-        console.log(error);
-    }
+    const response = runInstruction(data[0], 0, data);
+    console.log(response);
 };
-// puzzleOne();
+puzzleOne();
 
 const msg = {
     finished: 'finished',
     inLoop: 'in loop',
 };
 
-const runInstruction = (line, i, data, res = { acc: 0, msg: null }) => {
+const runInstruction = (line, i, data, visited = new Map(), res = { acc: 0, msg: null }) => {
     if (res.msg) {
         return res;
     }
@@ -37,21 +32,39 @@ const runInstruction = (line, i, data, res = { acc: 0, msg: null }) => {
     const { ins, dir, num } = line;
     switch (ins) {
         case 'nop': {
-            return runInstruction(data[i + 1], i + 1, data, res);
+            return runInstruction(data[i + 1], i + 1, data, visited, res);
         }
         case 'acc': {
             res.acc += dir === 1 ? +num : -num;
-            return runInstruction(data[i + 1], i + 1, data, res);
+            return runInstruction(data[i + 1], i + 1, data, visited, res);
         }
         case 'jmp': {
             const next = dir === 1 ? i + num : i - num;
-            return runInstruction(data[next], next, data, res);
+            return runInstruction(data[next], next, data, visited, res);
         }
     }
 };
 
 const findFix = async () => {
     const data = await getData(input).then(transform);
-    console.log(data);
+    const potentialFixes = new Map();
+    data.forEach((line, i) => {
+        if (['nop', 'jmp'].includes(line.ins)) {
+            potentialFixes.set(i, line);
+        }
+    });
+    for (let [i, fix] of potentialFixes) {
+        const newData = Array.from(data);
+        newData[i] = { ...fix, ins: fix.ins === 'nop' ? 'jmp' : 'nop' };
+        // console.log(i, newData[i], data[i]);
+        const res = runInstruction(newData[0], 0, newData);
+        if (res.msg === msg.inLoop) {
+            continue;
+        }
+        if (res.msg === msg.finished) {
+            console.log(res);
+            break;
+        }
+    }
 };
 findFix();
